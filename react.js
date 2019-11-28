1a929034c84bed9aa21f592e7da010775fc26314
Martin Zagora <zaggino@gmail.com>
2019-11-28 11:52:11 +0100

    v3.1.7

--- react.js
+++ react.js
@@ -13,7 +13,8 @@ const eslintConfig = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'react-app'
   ],
   settings: {
     react: {
